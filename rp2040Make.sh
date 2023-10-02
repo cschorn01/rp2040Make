cf07e28c328ff5ff1182f945f76fcfc277e0c886
Chris Schorn <42622387+cschorn01@users.noreply.github.com>
2023-10-02 13:43:48 -0400

    Update rp2040Make.sh

--- rp2040Make.sh
+++ rp2040Make.sh
@@ -6,8 +6,8 @@ rm -r build
 echo mkdir build
 build
 
-echo cmake -D FREERTOS_KERNEL_PATH=/Absolute/Path/To/FreeRTOS-Kernel -D PICO_SDK_PATH=/Absolute/Path/To/pico-sdk -B ./build
-cmake -D FREERTOS_KERNEL_PATH=/Absolute/Path/To/FreeRTOS-Kernel -D PICO_SDK_PATH=/Absolute/Path/To/pico-sdk -B ./build
+echo cmake -D PICO_SDK_PATH=/Absolute/Path/To/pico-sdk -B ./build
+cmake -D PICO_SDK_PATH=/Absolute/Path/To/pico-sdk -B ./build
 
 echo make -C build
 make -C build
