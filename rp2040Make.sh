52ddac1f0eb2b39a5042cd0b7554457e5cd80b63
Chris Schorn <42622387+cschorn01@users.noreply.github.com>
2023-10-02 15:57:49 -0400

    Update rp2040Make.sh

--- rp2040Make.sh
+++ rp2040Make.sh
@@ -4,7 +4,7 @@ echo rm -r build
 rm -r build
 
 echo mkdir build
-build
+mkdir build
 
 echo cmake -D PICO_SDK_PATH=/Absolute/Path/To/pico-sdk -B ./build
 cmake -D PICO_SDK_PATH=/Absolute/Path/To/pico-sdk -B ./build
